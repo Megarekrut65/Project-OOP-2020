551214fd5bb509895eeb3614c78550f58bc285d6
Олександр Бандалак <obandalk@hotmail.com>
2020-11-08 19:27:53 +0200

    Added change armors and names

--- Three ways/Assets/Scripts/Select/ReadAvatars.cs
+++ Three ways/Assets/Scripts/Select/ReadAvatars.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadAvatars : MonoBehaviour
 {
     private PlayerInfo player;
     public string accountPath;
     public GameObject[] objects;
+    public Image[] attacks; 
+    public Image[] protects; 
+    public string[] avatarNames;
+    public Image attack;
+    public Image protect;
+    public Text avatarName;
     private int currentIndex = 0;
     private int minIndex = 0;
     private int maxIndex = 1;
@@ -24,25 +31,32 @@ public class ReadAvatars : MonoBehaviour
             objects[i].SetActive(false);
         }
         objects[currentIndex].SetActive(true);
+        SetAll();
     }
-    public void Left()
+    void SetAll()
+    {
+        objects[currentIndex].SetActive(true);
+        objects[currentIndex].GetComponent<Avatar>().SetSet();
+        attack.sprite = attacks[currentIndex].sprite;
+        protect.sprite = protects[currentIndex].sprite;
+        avatarName.text = avatarNames[currentIndex];
+    }
+    public void Right()
     {
         if(currentIndex < maxIndex)
         {
             objects[currentIndex].SetActive(false);
             currentIndex++;
-            objects[currentIndex].SetActive(true);
-            objects[currentIndex].GetComponent<Avatar>().SetSet();
+            SetAll();     
         }
     }
-    public void Right()
+    public void Left()
     {
         if(currentIndex > minIndex)
         {
             objects[currentIndex].SetActive(false);
             currentIndex--;
-            objects[currentIndex].SetActive(true);
-            objects[currentIndex].GetComponent<Avatar>().SetSet();
+            SetAll();   
         }
     }
 }
