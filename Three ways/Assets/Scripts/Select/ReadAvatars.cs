3f625881c828554dbaf287243fb59c9c66a001cb
Олександр Бандалак <obandalk@hotmail.com>
2020-11-08 18:13:54 +0200

    Added animation for change avarats

--- Three ways/Assets/Scripts/Select/ReadAvatars.cs
+++ Three ways/Assets/Scripts/Select/ReadAvatars.cs
@@ -9,26 +9,40 @@ public class ReadAvatars : MonoBehaviour
     public GameObject[] objects;
     private int currentIndex = 0;
     private int minIndex = 0;
-    private int maxIndex = 2;
+    private int maxIndex = 1;
 
     void Start()
     {
         CorrectPathes.MakeCorrect(ref accountPath);
         player = new PlayerInfo(accountPath);
+        EditAvatars();
     }
-
+    void EditAvatars()
+    {
+        for(int i = 0; i <= maxIndex; i++)
+        {
+            objects[i].SetActive(false);
+        }
+        objects[currentIndex].SetActive(true);
+    }
     public void Left()
     {
         if(currentIndex < maxIndex)
         {
+            objects[currentIndex].SetActive(false);
             currentIndex++;
+            objects[currentIndex].SetActive(true);
+            objects[currentIndex].GetComponent<Avatar>().SetSet();
         }
     }
     public void Right()
     {
         if(currentIndex > minIndex)
         {
+            objects[currentIndex].SetActive(false);
             currentIndex--;
+            objects[currentIndex].SetActive(true);
+            objects[currentIndex].GetComponent<Avatar>().SetSet();
         }
     }
 }
